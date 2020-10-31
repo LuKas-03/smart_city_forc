d8e222dbcb38ab30c03e0f6153f99a1e8b29a428
kasimow.nur <kasimow.nur@gmail.com>
2020-10-31 14:27:10 +0500

    parse report

--- city69_backend/routes/dev.js
+++ city69_backend/routes/dev.js
@@ -1,10 +1,22 @@
-var express = require('express');
-var stat_gibdd = require('../integrations/stat_gibdd');
-var router = express.Router();
+const express = require('express');
+const fs = require('fs');
+const stat_gibdd = require('../integrations/stat_gibdd');
+const report_parser = require('../parsers/report-parcer');
+const report_processor = require('../parsers/report-processor');
+const router = express.Router();
 
 router.get('/gibdd', ((req, res) => {
   stat_gibdd(["1"], "MONTHS:9.2020", ["71140"], ["71171"], 
     (result) => res.send(result)
+  );
+}))
+
+router.get('/parse', ((req, res) => {
+  const fsStram = fs.createReadStream('Сельский жилищный фонд.csv')
+    .on('error', err => console.log(`[FILE READ ERROR] ${err}`));
+    
+    report_parser(fsStram, 'csv', (report) => report).then(
+    result => res.send(report_processor('БЛАГОУСТРОЙСТВО ЖИЛИЩНОГО ФОНДА', result))
   );
 }))
 
