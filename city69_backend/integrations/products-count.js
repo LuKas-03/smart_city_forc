0e94c70114406229f8fd43464580e5d70cb8fec9
Nur Kasimov <nur.kas99@ya.ru>
2020-11-28 17:41:26 +0500

    add new integration

--- city69_backend/integrations/products-count.js
+++ city69_backend/integrations/products-count.js
@@ -1,9 +1,10 @@
 const fetch = require('node-fetch');
 const HTMLParser = require('node-html-parser');
+const avitoUrl = 'https://www.avito.ru/';
 
 const getProductsCount = function(sity) {
     return new Promise(function(resolve, reject) {
-        fetch('https://www.avito.ru/'+sity)
+        fetch(avitoUrl+sity)
             .then(res => res.text())
             .then(body => parse(body))
             .then(result => resolve(result), error => reject(error));
